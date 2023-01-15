3da30d18d6bcc5ce3402a75dd338f874d91fba83
andreitoledo <andreiltoledo@hotmail.com>
2023-01-14 21:35:43 -0300

    Configurando o acesso ao banco de dados

--- Switch.API/Startup.cs
+++ Switch.API/Startup.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Switch.Infra.Data.Context;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,18 +14,31 @@ using System.Threading.Tasks;
 namespace Switch.API
 {
     public class Startup
-    {
+    {       
+
+        public IConfiguration Configuration { get; }
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
-        }
 
-        public IConfiguration Configuration { get; }
+            // passando para o configuration builder a string de conexão.
+            var builder = new ConfigurationBuilder().AddJsonFile("config.json");
+            Configuration = builder.Build();
+
+        }        
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+
+            // passando o nome da string de conexão para fazer referencia.
+            var conn = Configuration.GetConnectionString("SwitchDB");
+            // informa o contexto, informa em qual projeto esta o EF e adiciona o MVC Core ao trabalho.
+            services.AddDbContext<SwitchContext>(option => option.UseLazyLoadingProxies()
+                                                    .UseMySql(conn, m => m.MigrationsAssembly("Switch.Infra.Data")));
+            services.AddMvcCore();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
